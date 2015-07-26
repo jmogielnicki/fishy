b4c46c64c1765d29daf7238267298421f774b73d
John Mogielnicki <Lion@MacBook-Pro.local>
2015-07-25 22:26:22 -0700

    create fishy movement

--- sketch.js
+++ sketch.js
@@ -1,41 +1,59 @@
 var canvas;
-var bug;  // Declare object
-var bugList = [];
-var anchorList = [];
+var worm;
+var wormList = [];
 var drawButton;
 var eraseAllButton;
-var anchorButton;
 var randomSlider;
 var eraser = false;
-var anchorOn = false;
 var onCanvas;
 var randomness = 100;
 var switchModeButton;
+var approachButton;
+var switchDirectionButton;
+var facingForward = true;
+var approach = false;
 var modeCurrent = 0;
 var firstTimeSwitch = true;
-var activeAnchorX = 200;
-var activeAnchorY = 200;
+var wormSegmentSize = 10;
+var wormSizeOff = 0;
+var wormGrowthFactor= 1;
+var wormStopGrowing = false;
+var wormRed = 0;
+var wormGreen = 50;
+var wormBlue = 255;
+var wormSize = 100;
+var wormGrow = true;  
+var onTail;
+var approachSpeed;
+
+
+
 
 
 function setup() {
-  canvas = createCanvas(800, 500);
+  canvas = createCanvas(1000, 600);
   canvas.parent('canvasContainer');
   canvas.mouseOver(function() { onCanvas = true; });
   canvas.mouseOut( function() { onCanvas = false; });
-  // Create object
-  bug = new Jitter();
-  randomSlider = createSlider(1, 1000, 200);
-  randomSlider.parent('controls');
-  randomSlider.html("test");
-  anchorButton = createButton("Set Anchor");
-  anchorButton.parent('controls');
-  anchorButton.mousePressed(function() { anchorOn = true; });
+  worm = new Worm();
+  // randomSlider = createSlider(1, 100, 10);
+  // randomSlider.parent('controls');
+  // randomSlider.html("test");
   switchModeButton = createButton("Switch Mode");
   switchModeButton.parent('controls');
   switchModeButton.mousePressed(function() { modeCurrent++; });
-  eraseAllButton = createButton("Reset");
-  eraseAllButton.parent('controls');
-  eraseAllButton.mousePressed(eraseEverything)
+  approachButton = createButton("Release");
+  approachButton.parent('controls');
+  approachButton.mousePressed(function() { approach = true; })
+
+  switchDirectionButton = createButton("Turn Around");
+  switchDirectionButton.parent('controls');
+  switchDirectionButton.mousePressed(function() { 
+    facingForward = !facingForward; 
+    reverse(wormList);
+  })
+
+
   drawButton = createButton("Eraser");
   drawButton.parent('controls');
   drawButton.mousePressed(
@@ -48,34 +66,37 @@ function setup() {
     }
 
     eraser = !eraser;
-
   });
-  // canvas.position(100,100);
+  eraseAllButton = createButton("Reset");
+  eraseAllButton.parent('controls');
+  eraseAllButton.mousePressed(eraseEverything)
 }
 
 function draw() {
   changeMode();
-  randomness = randomSlider.value();
-  if (eraser === false && anchorOn === false && mouseIsPressed && onCanvas === true) {
-    bugList.push(new Jitter());
-  } else if (eraser === false && anchorOn === true && mouseIsPressed && onCanvas === true) {
-    anchorList.push(new Anchor());
+  // randomness = randomSlider.value();
+  if (eraser === false && mouseIsPressed && onCanvas === true && wormStopGrowing === false) {
+    wormList.unshift(new Worm());
+  } 
+  if (keyIsPressed) {
+    ;
   }
-  for (i = 0; i < bugList.length; i++) {
-    bug = bugList[i];
-    bug.move();
-    bug.display();
-    if  (eraser === true && mouseIsPressed && dist(bug.x, bug.y, mouseX, mouseY) < 10){
-      bugList.splice(i, 1);
+
+  for (i = 0; i < wormList.length; i++) {
+    worm = wormList[i];
+    worm.move();
+    worm.display();
+    if  (eraser === true && mouseIsPressed && dist(worm.x, worm.y, mouseX, mouseY) < 10){
+      wormList.splice(i, 1);
     }
   }
-  for (i = 0; i < anchorList.length; i++) {
-    anchor = anchorList[i];
-    anchor.display();
-    if  (eraser === true && mouseIsPressed && dist(anchor.x, anchor.y, mouseX, mouseY) < 10){
-      anchorList.splice(i, 1);
-    }
-  }
+}
+
+function mouseReleased() {
+  wormStopGrowing = false;
+  wormBlue = random(255);
+  wormGreen = random(255);
+  wormRed = random(255);
 }
 
 function changeMode() {
@@ -83,102 +104,101 @@ function changeMode() {
     modeCurrent = 0;
     firstTimeSwitch = true;
   } else if (modeCurrent === 0) {
-    background(50, 89, 100);
+    background(15, 55, 77);
   } else if (modeCurrent === 1) {
     fill(255, 10); // semi-transparent white
     rect(-1, -1, width+2, height+2);
   } else if (modeCurrent === 2) {
     if (firstTimeSwitch === true) {
-      background(255,255,255);
+      background(0,0,0);
       firstTimeSwitch = false;
     }
     noStroke();
-    fill(255, 10); 
+    fill(0, 5); 
     rect(0, 0, width, height);
 
-    fill(0,100,255, 10); 
+    fill(255,255,0, 100); 
   }
 };
 
 function eraseEverything() {
   modeCurrent = 0;
-  bugList = [];
-  anchorList = [];
+  wormList = [];
 }
 
-function Anchor() {
+// Worm class
+function Worm() {
   this.x = mouseX;
   this.y = mouseY;
-  this.diameter = 50;
-  var anchorRedFill = 154;
-  var goUp = true;
+  this.diameter = wormSegmentSize;
+  var attracted = false;
+  var moveXOff = 0;
+  var moveYOff = 500;
+  var xTime = 0.01;
+  var yTime = 0.005;
 
-  this.display = function() {
+  wormRed = wormRed + 10
+  wormBlue = wormBlue -10
+  this.blue = wormBlue;
+  this.red = wormRed;
+  this.green = wormGreen;
+  wormSegmentSize = wormSegmentSize + wormGrowthFactor;
+  wormSizeOff = wormSizeOff + 0.01;
+  if (wormGrowthFactor > 8) {
+    wormGrow = false;
+  } else if (wormGrowthFactor < -8) {
+    wormGrow = true;
+    onTail = true;
+  }
+  if (wormGrow === true) {
+    wormGrowthFactor = wormGrowthFactor + 2;
+  } else if (wormGrow === false) {
+    wormGrowthFactor = wormGrowthFactor - 2;
+  }
+  if (wormGrowthFactor > 8 && onTail === true) {
+    wormStopGrowing = true;
+    wormSegmentSize = 10;
+    // moveXOff = moveXOff + 100000;
 
-    if (anchorRedFill > 154) {
-      goUp = false;
-    } else if (anchorRedFill < 1) {
-      goUp = true;
-    }
-
-    if (goUp === true) {
-      anchorRedFill = anchorRedFill+3
-    } else {
-      anchorRedFill = anchorRedFill-3
-    }
-
-    fill(anchorRedFill, 0, 0);
-    this.diameter = (anchorRedFill/20)+30;
-    ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 
-}
 
-// Jitter class
-function Jitter() {
-  this.x = mouseX;
-  this.y = mouseY;
-  this.diameter = random(10, 30);
-  var attracted = false;
 
 
   this.move = function() {
-    var moveRandomness = randomness*abs(randomGaussian());
-    var moveRandomness = randomness*abs(randomGaussian());
-    var prevDistance = -1;
-    var attracted = false;
-    // print(anchorList.length); 
-    for (var i = 0; i < anchorList.length; i++) {
-      anchor = anchorList[i];
-      var currDistance = dist(this.x, this.y, anchor.x, anchor.y);
-      if (prevDistance === -1 || currDistance <= prevDistance) {
-        activeAnchorX = anchor.x;
-        activeAnchorY = anchor.y;
-        prevDistance = currDistance;
-        if(currDistance<200) {
-          attracted = true;
-        }
-      }
-    }
-    if(anchorList.length === 0) {
-      activeAnchorX = this.x;
-      activeAnchorY = this.y;
-    }
+    moveXOff = moveXOff + xTime;
+    moveYOff = moveYOff + yTime;
 
-    if (attracted === true) {
-      this.x = (this.x*200 + ((activeAnchorX)) + (random(-moveRandomness, moveRandomness)*100))/(301);
-      this.y = (this.y*200 + ((activeAnchorY)) + (random(-moveRandomness, moveRandomness)*100))/(301);
-    } else {
-      this.x = (this.x*100 + random(-moveRandomness, moveRandomness))/(100);
-      this.y = (this.y*100 + random(-moveRandomness, moveRandomness))/(100);
-    }
+    var xMoveRandomness = noise(moveXOff)*(width);
+    var yMoveRandomness = noise(moveYOff)*(height);
+
+    this.x = xMoveRandomness;
+    this.y = yMoveRandomness;
+
+    // this.x = ((this.x*randomness) + xMoveRandomness)/(1+randomness);
+    // this.y = ((this.y*randomness) + yMoveRandomness)/(1+randomness);
 
 
   };
 
   this.display = function() {
-    // this.diameter = this.diameter + random(-randomness/100, randomness/100);
+    fill(this.red, this.green, this.blue);
+    stroke(0, 0, 0);
     ellipse(this.x, this.y, this.diameter, this.diameter);
+    if (approach === true) {
+      if (facingForward === true){
+        approachSpeed = 0.001;
+        xTime = xTime + 0.0000002
+        yTime = yTime + 0.000001
+      } else {
+        approachSpeed = -0.002;
+        xTime = xTime - 0.0000001;
+        // yTime = yTime - 0.000001;
+      }
+      this.diameter = this.diameter + (this.diameter * approachSpeed)
+      // approachSpeed = approachSpeed + (0.0000001)
+    }
+
 
   }
 };
